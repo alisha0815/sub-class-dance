4c1713cfbc6ff62df30d3608c23a5d3acc961902
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2022-01-16 12:11:52 +0100

    es6 class implemented regular dancer added

--- scripts/es6.js
+++ scripts/es6.js
@@ -1,8 +1,44 @@
+'use-strict';
 
 $(() => {
-
   class Dancer {
-
+    constructor(top, left) {
+      this.top = top;
+      this.left = left;
+      this.$node = $('<div class="dancer"></div>');
+    }
+    setPosition(top, left) {
+      this.$node.css({ top, left });
+    }
   }
 
+  // random position
+  const randomStagePosition = function () {
+    const pos = [];
+    let topCoor = Math.floor(Math.random() * $('#stage').height());
+    let leftCoor = Math.floor(Math.random() * $('#stage').width());
+    pos.push(topCoor, leftCoor);
+    return pos;
+  };
+
+  const dancer = new Dancer(...randomStagePosition());
+  console.log(dancer.setPosition);
+
+  // sub-classes
+  // regular dancer instace
+
+  // tapper
+  // rainbow dancer
+
+  // function for regular
+  const handlerRegular = () => {
+    [topCoor, leftCoor] = randomStagePosition();
+    const regularDancer = new Dancer(topCoor, leftCoor);
+    $('#stage').append(regularDancer.$node);
+    regularDancer.setPosition(topCoor, leftCoor);
+  };
+
+  // // event-handler
+  // // regular
+  $('#regular').click(handlerRegular);
 });
